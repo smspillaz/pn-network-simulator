75fc7cc53c6d08dac85430b253f4c1394dab5c45
Sam Spilsbury <smspillaz@gmail.com>
2020-11-22 01:13:49 +0200

    HomePage: Use Number.isNaN

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -375,7 +375,7 @@ const NumericInput = ({ onChange }) => {
       type="text"
       style={{ width: '20px '}}
       defaultValue="0"
-      onChange={e => Number.parseInt(e.target.value) !== NaN ? onChange(Number.parseInt(e.target.value)) : null }></input>
+      onChange={e => !Number.isNaN(Number.parseInt(e.target.value)) ? onChange(Number.parseInt(e.target.value)) : null }></input>
   )
 };
 
