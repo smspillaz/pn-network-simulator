bfdad820362a4db2e6cb506e4225a66514584626
Sam Spilsbury <smspillaz@gmail.com>
2020-11-22 01:13:41 +0200

    HomePage: Remove unused Node

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -58,53 +58,6 @@ const filterValidJSArray = (str, next) => {
 const fontSize = 14;
 const radius = 15;
 
-const Node = ({ node }) => {
-  // sizes
-  const sizes = {
-    radius: radius,
-    textSize: fontSize,
-    textX: radius * 1.5,
-    textY: radius / 2,
-  };
-  const sizesImg = {
-    radius: 30,
-    textSize: fontSize,
-    textX: 30 * 1.5,
-    textY: 30 / 2,
-  };
-
-  return (
-    <>
-      {node.img ? (
-        <image
-          href={node.img}
-          x="0"
-          y="0"
-          height={sizesImg.radius * 2}
-          width={sizesImg.radius * 2}
-          style={{
-            transform: `translate(-${sizesImg.radius}px, -${sizesImg.radius}px)`,
-          }}
-        />
-      ) : (
-        <circle fill={`lightblue`} r={sizes.radius} />
-      )}
-      <g style={{ fontSize: sizes.textSize + 'px' }}>
-        <text
-          x={node.img ? sizesImg.radius + 7 : -sizesImg.textSize / 2}
-          y={
-            node.img
-              ? sizesImg.radius / 2 - sizesImg.textSize
-              : sizes.radius / 2
-          }
-        >
-          {node.label}
-        </text>
-      </g>
-    </>
-  );
-};
-
 const NodeTable = ({
   nodeInitialStates,
   nodeInitialStateChanged,
